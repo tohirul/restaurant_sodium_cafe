bb7b87e1bc93104fdc1bbfaa6752797e0dd6fd37
Tohirul Islam <tohirul.islam164@gmail.com>
2022-10-22 21:14:43 +0600

    Nav Footer

---
+++ javaScript/main.js
@@ -0,0 +1,25 @@
+// @ts-nocheck
+const navIconEl = document.querySelector(".nav__icon");
+const navCloseEl = document.querySelector(".nav__close");
+const navList = document.querySelector(".nav__list");
+const navBgOverlayEl = document.querySelector(".nav__bgOverlay");
+const body = document.getElementsByTagName("body");
+const navOpen = () => {
+    navList?.classList.add("show");
+    navBgOverlayEl?.classList.add("active");
+    document.body.style =
+        "visibility:visible;height:100vh;width:100vw;overFlow:hidden;";
+};
+
+const navClose = () => {
+    navList?.classList.remove("show");
+    navBgOverlayEl?.classList.remove("active");
+    document.body.style =
+        "visibility:visible;height:initial;width:100%;overFlow-x:hidden;";
+};
+const check = () => console.log("BodyBuilder");
+
+navIconEl?.addEventListener("click", navOpen);
+navCloseEl?.addEventListener("click", navClose);
+navBgOverlayEl?.addEventListener("click", navClose);
+body[0].addEventListener("click", check);
